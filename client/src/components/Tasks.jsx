138027e697b4a2ab6d9d7d60996b67a36f413cce
WalaNour <nourwala548@gmail.com>
2020-10-03 15:24:08 +0100

    full app task manegment

--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-
 class Tasks extends React.Component {
   constructor(props) {
     super(props);
@@ -8,11 +7,11 @@ class Tasks extends React.Component {
       allEmployees: [],
       taskForEmployee: "",
       nameOfEmloyee: "",
-      newEmploye:"",
-      fire : "" ,
-      archive : []
+      newEmploye: "",
+      fire: "",
+      archive: [],
     };
-    this.getAllArchive = this.getAllArchive.bind(this)
+    this.getAllArchive = this.getAllArchive.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.getAllEmployees = this.getAllEmployees.bind(this);
   }
@@ -22,107 +21,108 @@ class Tasks extends React.Component {
   }
   // send the task to the employee
   sendToEmployee() {
-    axios.post("/api/users/giveOrder", {
-      taskForEmployee: this.state.taskForEmployee,
-      nameOfEmloyee: this.state.nameOfEmloyee,
+    axios
+      .post("/api/users/giveOrder", {
+        taskForEmployee: this.state.taskForEmployee,
+        nameOfEmloyee: this.state.nameOfEmloyee,
+      })
+      .then(() => {
+        this.componentWillMount();
+      });
+  }
+  fireEmployee() {
+    axios.post("/api/user/fire", { newF: this.state.fire });
+  }
+  addEmployee() {
+    axios.post("/api/user/add", { newE: this.state.newEmploye }).then((res) => {
+      this.setState({ allEmployees: [...this.state.allEmployees, res.data] });
     });
   }
-  fireEmployee(){
-    axios.post("/api/user/fire" , {newF : this.state.fire}) 
-  }
-  addEmployee(){
-    axios.post("/api/user/add" , {newE : this.state.newEmploye})
-  }
-  
   // get the list of the employees from the database
   getAllEmployees() {
     $.ajax({
       url: "/api/users/employee",
       type: "get",
       success: (res) => {
-        this.setState({ allEmployees: res })
+        this.setState({ allEmployees: res });
       },
     });
   }
-
   getAllArchive() {
     $.ajax({
       url: "/api/users/archive",
       type: "get",
       success: (res) => {
-        var arch = [] 
-        for(var i = 0 ; i < res.length ; i++){
-        arch.push(<li key={i}>to {res[i].name} : {res[i].todo}</li>)
+        var arch = [];
+        for (var i = 0; i < res.length; i++) {
+          arch.push(
+            <li key={i}>
+              to {res[i].name} : {res[i].todo}
+            </li>
+          );
         }
         this.setState({ archive: arch });
       },
     });
   }
   // async getAllArchive(){
-   
   //     const data =await fetch("/api/users/archive");
   //   let archiive= await  data.json();
   //   console.log(archiive)
-    
-    
   // }
   componentWillMount() {
-    this.getAllArchive()
-    this.addEmployee()
-    this.getAllArchive()
+    this.getAllArchive();
+    this.addEmployee();
+    this.getAllArchive();
     this.getAllEmployees();
   }
   render() {
     var employeesList = [];
     for (var i = 0; i < this.state.allEmployees.length; i++) {
-      employeesList.push(
-        <option key={i}>{this.state.allEmployees[i]}</option>
-      );
+      employeesList.push(<option key={i}>{this.state.allEmployees[i]}</option>);
     }
-    console.log("helloo " ,this.state.archive)
-
+    console.log("helloo ", this.state.archive);
     return (
       <div>
         <div className="Orders">
           <p>
-            Choose HERE :
+           Pick Employee
             <select
               placeholder="WORKERS NAMES"
               name="nameOfEmloyee"
               onChange={this.handleChange}
             >
               {employeesList}
-              <option>-- Select ALL -- </option>
+              <option>-- Select -- </option>
             </select>
           </p>
           <p>
-            DO WHAT ?
+            Require Task
             <input
+               id="task"
               name="taskForEmployee"
               type="text"
-              placeholder="TODO"
+              placeholder="Task"
               onChange={this.handleChange}
             ></input>
             <button onClick={this.sendToEmployee.bind(this)}>send</button>
           </p>
         </div>
         <div className="companyTodo">
-          <ul>
-            {this.state.archive}
-          </ul>
+          <ul>{this.state.archive}</ul>
         </div>
         <div>
-          <h3>add new employee</h3>
+          <h3>Add Newcomer</h3>
           <input name="newEmploye" onChange={this.handleChange}></input>
-          <button onClick={this.addEmployee.bind(this)}>add one</button>
+          <button onClick={this.addEmployee.bind(this)}>Add</button>
         </div>
         <div>
-          <h3>fire employe</h3>
+          <h3>Dismiss</h3>
           <input name="fire" onChange={this.handleChange}></input>
-          <button onClick={this.fireEmployee.bind(this)}>FIRE HIM</button>
+          <button onClick={this.fireEmployee.bind(this)}>Confirm</button>
         </div>
       </div>
     );
   }
 }
-export default Tasks
+export default Tasks;
